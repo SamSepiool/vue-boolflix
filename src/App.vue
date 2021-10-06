40c12e301e4a9f696afd3907f8d5a6c45e53fef7
SamSepiool <simone.severini@outlook.it>
2021-10-06 16:37:48 +0200

    add Series component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
   <div id="app">
     <Header @passInput="getQuery"/>
     <Films :movies='movies'/>
+    <Series :tvSeries='tvSeries'/>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Films from "./components/Films.vue";
+import Series from "./components/Series.vue";
+
 
 import axios from "axios";
 
@@ -16,25 +19,29 @@ export default {
   components: {
 
     Header,
-    Films
+    Films,
+    Series 
    
   },
   data() {
     return{
       passedInput: '',
-      movies: []
+      movies: [],
+      tvSeries: []
       }
   },
   methods: {
 
     getQuery: function(inputValue){
 
-      if(inputValue == ''){
+      if (inputValue == '') {
         this.movies = []
-      }else{
+        this.tvSeries = []
+      } else {
 
         this.passedInput = inputValue;
-
+        
+        // MOVIES API CALL
         axios.get('https://api.themoviedb.org/3/search/movie', {
           params: {
             api_key: '24da6980da0cf63e4d4cc8af46e62ed8',
@@ -45,6 +52,19 @@ export default {
           .then ((res) => {
             this.movies = res.data.results
             console.log(this.movies)
+        })
+
+        // SERIES API CALL 
+        axios.get('https://api.themoviedb.org/3/search/tv', {
+          params: {
+            api_key: '24da6980da0cf63e4d4cc8af46e62ed8',
+            query: this.passedInput,
+            language: 'it-IT'
+          }
+          })
+          .then ((res) => {
+            this.tvSeries = res.data.results
+            console.log(this.tvSeries)
         })
       } 
     }
---
+++ src/components/Series.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <div class="wrapper">
+        <h2>Series</h2>
+
+        <ul>
+            <li v-for="(series, index) in tvSeries" :key="index"> 
+                TITLE: {{series.name}}
+                ORIGINAL TITLE: {{series.name}}
+                LANG: {{series.original_language}}
+                VOTO: {{series.vote_average}}
+            </li>
+        </ul>
+    </div>
+  
+</template>
+
+<script>
+
+export default {
+    name: 'Series',
+    props:{
+        tvSeries: Array,
+    },
+
+}
+</script>
+
+<style>
+
+</style>
