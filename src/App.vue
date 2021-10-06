a4385749cb0cd129fe99463b7e8996c3f05aad26
SamSepiool <simone.severini@outlook.it>
2021-10-06 15:57:07 +0200

    add getQuery method / API call

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
-
+    <Header @passInput="getQuery"/>
   </div>
 </template>
 
 <script>
-
+import Header from "./components/Header.vue";
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
+
+    Header,
    
+  },
+  data() {
+    return{
+      passedInput: '',
+      movies: []
+      }
+  },
+  methods: {
+
+    getQuery: function(inputValue){
+
+      this.passedInput = inputValue;
+
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: '24da6980da0cf63e4d4cc8af46e62ed8',
+          query: this.passedInput,
+          language: 'it-IT'
+        }
+      })
+      .then ((res) => {
+        this.movies = res.data.results
+        console.log(this.movies)
+      })
+    }
   }
+  
+
 }
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+      <input type="text" v-model='searchInput'>
+      <button @click="$emit('passInput', searchInput)">Cerca</button>
+  </header>
+</template>
+
+<script>
+export default {
+
+    name: 'Header',
+
+    data(){
+        return{
+
+            searchInput: ''
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
