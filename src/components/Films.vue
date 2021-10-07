5b7a832340f9c0cfbc70fb44912da4e59ed1d07c
SamSepiool <simone.severini@outlook.it>
2021-10-07 15:20:45 +0200

    milestone 3 done

--- src/components/Films.vue
+++ src/components/Films.vue
@@ -5,11 +5,17 @@
 
         <ul>
             <li v-for="(movie, index) in movies" :key="index"> 
+
+                <img v-if="movie.poster_path != null" :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt="">
+                
                 TITLE: {{movie.title}}
                 ORIGINAL TITLE: {{movie.original_title}}
                 LANG: {{movie.original_language}}
                 <lang-flag :iso="movie.original_language" :squared="false"/>
                 VOTO: {{movie.vote_average}}
+                <font-awesome-icon v-for="(solid, index) in starsVote(movies)[index]" :key="index"   :icon='starSolid'/>
+                <font-awesome-icon v-for="(empty, index) in (5 - starsVote(movies)[index])" :key="`ciccio`+index"   :icon='starEmpty'/>
+
             </li>
         </ul>
     </div>
@@ -18,6 +24,9 @@
 
 <script>
 import LangFlag from 'vue-lang-code-flags';
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 export default {
     name: 'Films',
@@ -25,7 +34,30 @@ export default {
         movies: Array,
     },
     components:{
-        LangFlag
+        LangFlag,
+        FontAwesomeIcon
+    },
+    data(){
+        return {
+            starSolid: fasStar,
+            starEmpty: farStar,
+
+        }
+    },
+    methods:{
+        
+       starsVote(arr){
+           const stars = [];
+           arr.forEach((elm) => {
+
+               let parseInStars = Math.ceil(elm.vote_average / 2);
+               stars.push(parseInStars);
+               
+           })
+           return stars;
+       }
+
+        
     }
 
 }
--- src/components/Series.vue
+++ src/components/Series.vue
@@ -5,11 +5,15 @@
 
         <ul>
             <li v-for="(series, index) in tvSeries" :key="index"> 
+                <img v-if="series.poster_path != null" :src="`https://image.tmdb.org/t/p/w342/${series.poster_path}`" alt="">
+               
                 TITLE: {{series.name}}
                 ORIGINAL TITLE: {{series.name}}
                 LANG: {{series.original_language}}
                 <lang-flag :iso="series.original_language" :squared="false"/>
                 VOTO: {{series.vote_average}}
+                <font-awesome-icon v-for="(solid, index) in starsVote(tvSeries)[index]" :key="index"   :icon='starSolid'/>
+                <font-awesome-icon v-for="(empty, index) in (5 - starsVote(tvSeries)[index])" :key="`ciccio`+index"   :icon='starEmpty'/>
             </li>
         </ul>
     </div>
@@ -18,6 +22,10 @@
 
 <script>
 import LangFlag from 'vue-lang-code-flags';
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 
 
 export default {
@@ -27,11 +35,38 @@ export default {
     },
     components:{
         LangFlag,
+        FontAwesomeIcon
+
+    },
+    data(){
+        return {
+            starSolid: fasStar,
+            starEmpty: farStar,
+            stars: [],
+        }
+    },
+    methods:{
+
+         starsVote(arr){
+           const stars = [];
+           arr.forEach((elm) => {
+
+               let parseInStars = Math.ceil(elm.vote_average / 2);
+               stars.push(parseInStars);
+               
+           })
+           return stars;
+       }
+        
     }
 
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
+.far{
+    color:yellow;
+    font-size: 50px;
+}
 
 </style>
